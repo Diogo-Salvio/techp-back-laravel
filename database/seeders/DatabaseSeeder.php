dc0dd67026bd29ff486629f0edff6e8a08a81f05
Diogo Salvio <diogosalvio787@gmail.com>
2025-09-07 22:11:33 -0300

    refactor: remoção do código que não estava sendo utilizado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             MusicaSeeder::class,
-            MinhasMusicasSeeder::class,
         ]);
     }
 }
